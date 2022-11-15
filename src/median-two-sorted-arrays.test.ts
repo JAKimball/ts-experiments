3c5c57c205ac1add4398249a1e7d311ad342f473
Jonathan A. Kimball <14268609+JAKimball@users.noreply.github.com>
2022-11-14 20:27:40 -0800

    update @typescript-eslint packages

--- src/median-two-sorted-arrays.test.ts
+++ src/median-two-sorted-arrays.test.ts
@@ -1,16 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { assert, describe, expect, it } from 'vitest'
-
 import { findMedianSortedArrays } from './median-two-sorted-arrays.js'
+
 describe('"findMedianSortedArrays" function', () => {
-  it('case 1', () => {
+  it('Case 1', () => {
     const nums1 = [1, 3]
     const nums2 = [2]
-    expect(findMedianSortedArrays(nums1, nums2)).toEqual(2)
+    expect(findMedianSortedArrays(nums1, nums2)).toBe(2)
   })
+
   it('Case 2', () => {
     const nums1 = [1, 2]
     const nums2 = [3, 4]
-    expect(findMedianSortedArrays(nums1, nums2)).toEqual(2.5)
+    expect(findMedianSortedArrays(nums1, nums2)).toBe(2.5)
   })
 })
