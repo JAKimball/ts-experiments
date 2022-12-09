9f580cc7699d40b11a96daf3bf7525abb6b28995
Jonathan A. Kimball <14268609+JAKimball@users.noreply.github.com>
2022-12-09 11:02:20 -0800

    add readLn and initReadLn functions to lib

--- src/aoc/lib/index.ts
+++ src/aoc/lib/index.ts
@@ -1,3 +1,10 @@
 export const ints = (str: string) => {
   return [...str.matchAll(/-?\d+/g)].map(match => +match[0])
 }
+
+let inputIterator: IterableIterator<string>
+export const initReadLn = (input: string) => (inputIterator = input.split('\n').values())
+
+export const readLn = (pInputIterator?: IterableIterator<string>): string | undefined => {
+  return (pInputIterator ?? inputIterator).next().value
+}
